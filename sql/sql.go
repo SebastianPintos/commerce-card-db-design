5b87f2229e790c7d2f55337f8468a564ecc60b92
sol <solpierozzi@hotmail.com>
2020-06-03 00:15:05 -0300

    Actualiza pagado a true en generar resumen

--- sql/sql.go
+++ sql/sql.go
@@ -515,55 +515,66 @@ func GenerarResumen() {
 		`create or replace function generarResumen(cliente int, anioR int, mesR int) returns bool as $$
 		declare
 			   idResumen int;
-			   totalPagar decimal(7,2) := 0;
-
-				begin
+			   totalPagar decimal(8,2) := 0;
+			   _linea record;
+				
+		begin
 		-- 	Generar Cabecera
-				INSERT INTO cabecera (nombre, apellido, domicilio, nrotarjeta, desde, hasta, vence)
-				SELECT cli.nombre, cli.apellido, cli.domicilio, t.nrotarjeta, c.fechainicio, c.fechacierre, c.fechavto
-					FROM public.tarjeta t, public.cierre c, public.cliente cli
-					WHERE SUBSTRING (t.nrotarjeta, LENGTH(t.nrotarjeta), 1)::int = c.terminacion
-					and cli.nrocliente = t.nrocliente
-					and t.nrocliente = cliente
-					and c.año = anioR
-					and c.mes = mesR
-				RETURNING nroresumen INTO idResumen;
+		INSERT INTO cabecera (nombre, apellido, domicilio, nrotarjeta, desde, hasta, vence)
+		SELECT cli.nombre, cli.apellido, cli.domicilio, t.nrotarjeta, c.fechainicio, c.fechacierre, c.fechavto
+		FROM public.tarjeta t, public.cierre c, public.cliente cli
+		WHERE SUBSTRING (t.nrotarjeta, LENGTH(t.nrotarjeta), 1)::int = c.terminacion
+		and cli.nrocliente = t.nrocliente
+		and t.nrocliente = cliente
+		and c.año = anioR
+		and c.mes = mesR
+		RETURNING nroresumen INTO idResumen;
 
-				if (idResumen is null) then
+		if (idResumen is null) then
 					raise 'No se pudo generar el resumen, Cliente inexistente';
 					return False;
-				end if;
+		end if;
 
 		-- Generar detalle
-				INSERT INTO detalle (nroresumen, nrolinea, fecha, nombrecomercio, monto)
-				SELECT idResumen, ROW_NUMBER () OVER (ORDER BY t.nrotarjeta) as nrolinea, co.fecha, com.nombre , co.monto
-				FROM public.tarjeta t, public.cierre c, public.compra co, public.comercio com
-				WHERE SUBSTRING (t.nrotarjeta, LENGTH(t.nrotarjeta), 1)::int = c.terminacion
-				and co.nrotarjeta = t.nrotarjeta
-				and com.nrocomercio = co.nrocomercio
-				and t.nrocliente = cliente
-				and c.año = anioR
-				and c.mes = mesR
-				and co.fecha >= c.fechainicio
-				and co.fecha <= c.fechacierre;
+		INSERT INTO detalle (nroresumen, nrolinea, fecha, nombrecomercio, monto)
+		SELECT idResumen, ROW_NUMBER () OVER (ORDER BY t.nrotarjeta) as nrolinea, co.fecha, com.nombre , co.monto
+			FROM public.tarjeta t, public.cierre c, public.compra co, public.comercio com
+			WHERE SUBSTRING (t.nrotarjeta, LENGTH(t.nrotarjeta), 1)::int = c.terminacion
+			and co.nrotarjeta = t.nrotarjeta
+			and com.nrocomercio = co.nrocomercio
+			and t.nrocliente = cliente
+			and c.año = anioR
+			and c.mes = mesR
+			and co.fecha >= c.fechainicio
+			and co.fecha <= c.fechacierre;
 
-				if (lastval() is NULL) then
-					raise 'No se pudo generar el resumen';
-					return False;
-				end if;
+		if (lastval() is NULL) then
+				raise 'No se pudo generar el resumen';
+				return False;
+		end if;
 
 		-- Actualizar Resumen
-				totalPagar := (SELECT SUM(monto)
+		totalPagar := (SELECT SUM(monto)
 							  FROM detalle
 							  WHERE nroresumen = idResumen
 							  GROUP BY nroresumen);
-				UPDATE cabecera
-				set total = totalPagar
-				WHERE nroresumen = idResumen;
+		UPDATE cabecera
+			set total = totalPagar WHERE nroresumen = idResumen;
 
-				return True;
+		--Cambiar pagado a True
+		for _linea in SELECT * FROM public.tarjeta t, public.cierre c, public.compra co, public.comercio com
+			WHERE SUBSTRING (t.nrotarjeta, LENGTH(t.nrotarjeta), 1)::int = c.terminacion
+			and co.nrotarjeta = t.nrotarjeta
+			and com.nrocomercio = co.nrocomercio
+			and t.nrocliente = cliente loop
 
-				   end;
+					UPDATE compra set pagado = True where nrotarjeta=_linea.nrotarjeta and monto=_linea.monto;				
+				
+		end loop;	
+				
+		return True;
+
+		end;
 		$$ language plpgsql;`)
 	logErr(err)
 }
